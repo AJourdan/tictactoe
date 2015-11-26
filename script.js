eab1eaf0ae9f77bfba40168b5139b698a243f661
Alon Jourdan <alon.jourdan@gmail.com>
2015-11-26 15:12:15 -0500

    Working X & O's

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
 $(document).on('ready', function() {
-	
+	var player1 = "player1";
+	var player2 = "player2";
+	var turn = "player1";
 
+
+
+	var turns = function() {
+		if (turn === "player1") { 	
+		$(this).text('x');
+			turn = "player2"
+		} else {
+			
+		$(this).text('O');	
+			turn = "player1"
+		}
+
+	};
+
+	$("td").on('click', turns);
 
 
 });

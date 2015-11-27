e361db070c4e198e8d8cc5719b174dae38106968
Alon Jourdan <alon.jourdan@gmail.com>
2015-11-26 19:49:45 -0500

    Popup Window for winner

--- script.js
+++ script.js
@@ -2,15 +2,16 @@ $(document).on('ready', function() {
 	var player1 = "player1";
 	var player2 = "player2";
 	var turn = "player1";
+	var winningCombo = [[1,2,3],[4,5,6],[7,8,9],[3,5,7],[1,5,9],[1,4,7],[2,5,8],[3,6,9]]
 
 
 
 	var turns = function() {
 		if (turn === "player1") { 	
-		$(this).text('x');
+		$(this).text('X');
 			turn = "player2"
 		} else {
-			
+
 		$(this).text('O');	
 			turn = "player1"
 		}
@@ -19,5 +20,28 @@ $(document).on('ready', function() {
 
 	$("td").on('click', turns);
 
+	var winner = function() {
+		$('td').off('click');
+		window.alert('winner');
+	}
+
+for (var i = winningCombo.length - 1; i >= 0; i--) {
+	winningCombo[i]
+
+	var id = winningCombo[i][0];
+	var moveOne = $('#' + id).text()
+
+	var id = winningCombo[i][1];
+	var moveTwo = $('#' + id).text()
+
+	var id = winningCombo[i][2];
+	var moveThree = $('#' + id).text()
+
+	if (moveOne === moveTwo && moveTwo === moveThree) {
+		winner();
+	}
+
+};
 
 });
+
